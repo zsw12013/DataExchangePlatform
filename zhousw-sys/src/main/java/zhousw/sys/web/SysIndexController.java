7e7c5b287a5ecefcf1bfdb6517bfad09bf5c415e
zsw12013 <zsw12013@126.com>
2016-12-21 23:18:29 +0800

    搭建

--- zhousw-sys/src/main/java/zhousw/sys/web/SysIndexController.java
+++ zhousw-sys/src/main/java/zhousw/sys/web/SysIndexController.java
@@ -1,11 +1,16 @@
 package zhousw.sys.web;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import zhousw.sys.entity.SysUser;
+import zhousw.sys.facade.SysUserFacade;
+
 /**
  * <B>系统名称：</B><BR>
  * <B>模块名称：</B><BR>
@@ -15,26 +20,29 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class SysIndexController {
-	
 
+    @Resource
+    private SysUserFacade sysUserFacade;
 
-	/**
+    /**
      * <B>方法名称：</B>系统首页<BR>
      * <B>概要说明：</B><BR>
-     * 
+     *
      * @param request 页面请求
      * @param response 页面响应
-      * @return ModelAndView 模型视图
+
+     * @return ModelAndView 模型视图
      */
     @RequestMapping("/sysindex.html")
     public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView ret = new ModelAndView();
-        
+        SysUser user = this.sysUserFacade.getUser();
+        System.out.println(user);
+
         return ret;
     }
-    
 
-    
-    
+
+
 
 }
